d85d2feb823f1bdae58e06b6c75e0f087886cb36
Fernando Chamussola <159538875+FernandoChamussola@users.noreply.github.com>
2025-09-30 17:20:15 +0200

    lancando a primeira versao

--- app.js
+++ app.js
@@ -2,38 +2,40 @@ import fs from 'fs-extra';
 import path from 'path';
 import color from 'chalk';
 import { fileURLToPath } from 'url';
+import { program } from 'commander';
 
 // Configurar __dirname para ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const projectName = process.argv[2];
+// Configuração do Commander
+program
+  .name('nxgo')
+  .description('Cria um novo projeto Next.js a partir de um template')
+  .version('1.0.0')
+  .argument('<projectName>', 'Nome do projeto a ser criado')
+  .action((projectName) => {
+    console.log(color.blue(`Criando o projecto ${projectName}`));
 
-if (!projectName) {
-    console.log(color.red("Por favor forneça o nome do projecto"));
-    process.exit(1);
-}
+    const templatePath = path.join(__dirname, './templates/nextV1');
+    const targetPath = path.join(process.cwd(), projectName);
 
-console.log(color.blue(`Criando o projecto ${projectName}`));
-
-const templatePath = path.join(__dirname, './templates/nextV1');
-const targetPath = path.join(process.cwd(), projectName);
-
-fs.copy(templatePath, targetPath)
-    .then(() => {
+    fs.copy(templatePath, targetPath)
+      .then(() => {
         console.log(color.green('Projecto criado com sucesso'));
         console.log(color.blue(`\nPróximos passos:\n`));
         console.log(color.cyan(`  cd ${projectName}`));
         console.log(color.cyan('  cd project'));
         console.log(color.cyan('  npm install'));
         console.log(color.cyan('  npm run dev\n'));
-        //agradecer por usar a ferramenta
         console.log(color.green('Obrigado por usar o nextjsTemplateCLI!'));
-        //creditos ao autor
         console.log(color.blue('Feito por Fernando Chamussola\n'));
-        process.exit(0);
-    })
-    .catch((err) => {
+      })
+      .catch((err) => {
         console.error(color.red(err));
         process.exit(1);
-    });
+      });
+  });
+
+// Parse dos argumentos
+program.parse(process.argv);
